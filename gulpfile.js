ff777aa8c76d58f460f328b2d9a4d13cf069dd10
Andrii Dvoiak <dvoyak@gmail.com>
2018-06-22 17:18:32 +0300

    Move to AMP

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var purify = require('gulp-purifycss');
+var cleanCSS = require('gulp-clean-css');
+var htmlreplace = require('gulp-html-replace');
+var amphtmlValidator = require('amphtml-validator');
+var fs = require('fs');
+
+const BUILD_PATH = './build';
+const BUILD_HTML = BUILD_PATH + '/index.html';
+const SOURCE = {
+  'BOOTSTRAP_CSS': './source/static/bootstrap-3.3.5-dist/css/bootstrap.min.css',
+  'AMPHTML': './views/pages/index-full.ejs',
+  'CLEANED_CSS': './amphtml/css/bootstrap.min.css'
+}
+
+// purify removes unused CSS classes
+gulp.task('purify', function() {
+  return gulp.src(SOURCE.BOOTSTRAP_CSS)
+    .pipe(purify([SOURCE.AMPHTML]))
+    .pipe(gulp.dest('./amphtml/css'));
+});
+
+// inline-css inserts the cleaned + minified CSS into HTML
+gulp.task('inline-css', ['purify'], function() {
+  return gulp.src(SOURCE.AMPHTML)
+    .pipe(htmlreplace({
+      'cssInline': {
+        'src': gulp.src(SOURCE.CLEANED_CSS).pipe(cleanCSS()),
+        'tpl': '<style amp-custom>%s</style>'
+      }
+    }))
+    .pipe(gulp.dest(BUILD_PATH));
+});
+
+// validate ensures the AMP HTML is valid
+gulp.task('validate', function() {
+  amphtmlValidator.getInstance().then(function (validator) {
+    var input = fs.readFileSync(BUILD_HTML, 'utf8');
+    var result = validator.validateString(input);
+    ((result.status === 'PASS') ? console.log : console.error)(BUILD_HTML + ": " + result.status);
+    for (var ii = 0; ii < result.errors.length; ii++) {
+      var error = result.errors[ii];
+      var msg = 'line ' + error.line + ', col ' + error.col + ': ' + error.message;
+      if (error.specUrl !== null) {
+        msg += ' (see ' + error.specUrl + ')';
+      }
+      ((error.severity === 'ERROR') ? console.error : console.warn)(msg);
+    }
+  });
+});
+
+// Build task cleans the CSS and inlines it
+gulp.task('build', ['inline-css']);
+// Default task will only validate the build output
+gulp.task('default', ['validate']);
+
+//https://www.uncompiled.org/blog/2016/amp-bootstrap/
+//
